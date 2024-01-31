3bb53f1b9cad79631875ef8a7c7c5f121e47218f
dyahnax <yahnadiever@gmail.com>
2024-01-31 08:11:46 +0700

    Coba 2
    Semangat awal semester 2!

--- GITHUB COBA 2/src/github/coba/pkg2/GITHUBCOBA2.java
+++ GITHUB COBA 2/src/github/coba/pkg2/GITHUBCOBA2.java
@@ -2,20 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package githubcoba;
-import java.util.scanner;
+package github.coba.pkg2;
+import java.util.Scanner;
 
 /**
  *
  * @author acer
  */
-public class GITHUBCOBA {
+public class GITHUBCOBA2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Dyahnaa");
+        Scanner input = new Scanner(System.in);
         
         
     }
